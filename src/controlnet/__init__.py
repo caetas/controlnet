170b8eb57fcd70608c02374e1cb5e68745f57809
caetas <up201705031@fe.up.pt>
2023-08-27 17:12:18 +0100

    Setup cookiecutter

---
+++ src/controlnet/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for controlnet.
+
+    ControlNet to generate medical images
+"""
+from version import __version__
+
+__author__ = "Francisco Caetano"
---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Unit test package for controlnet."""
+import os
+
+_TEST_ROOT = os.path.dirname(__file__)
+_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)
